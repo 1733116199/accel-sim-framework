bd0e02b3e2e8d4cf5e2774025950242e88e5c398
1733116199 <1733116199@qq.com>
2023-01-25 16:08:58 -0800

    update vector_add

--- myapps/vector_add.cu
+++ myapps/vector_add.cu
@@ -3,30 +3,37 @@
 
 #define N 1024
 
-__global__ void vector_add(float *a, float *b, float *c, int size) {
+__global__ void vector_add(float *a, float *b1, float * b2, float *c, int size) {
   int i = threadIdx.x;
   if (i < size) {
-    c[i] = a[i] + b[i];
+    if((i % 2) == 0)
+        c[i] = a[i] + b1[i];
+    else
+        c[i] = a[i] + b2[i];
   }
 }
 
 int main() {
   float a[N];
-  float b[N];
+  float b1[N];
+  float b2[N];
   float c[N];
   for (int i = 0; i < N; i++) {
     a[i] = i;
-    b[i] = 2 * i;
+    b1[i] = 2 * i;
+    b2[i] = 3 * i;
   }
-  float *a_device, *b_device, *c_device;
+  float *a_device, *b1_device, *b2_device, *c_device;
   cudaMalloc(&a_device, sizeof(a));
-  cudaMalloc(&b_device, sizeof(b));
+  cudaMalloc(&b1_device, sizeof(b1));
+  cudaMalloc(&b2_device, sizeof(b2));
   cudaMalloc(&c_device, sizeof(c));
 
   cudaMemcpy(a_device, a, sizeof(a), cudaMemcpyHostToDevice);
-  cudaMemcpy(b_device, b, sizeof(b), cudaMemcpyHostToDevice);
+  cudaMemcpy(b1_device, b1, sizeof(b1), cudaMemcpyHostToDevice);
+  cudaMemcpy(b2_device, b2, sizeof(b2), cudaMemcpyHostToDevice);
 
-  vector_add<<<1, N>>>(a_device, b_device, c_device, N);
+  vector_add<<<1, N>>>(a_device, b1_device, b2_device,c_device, N);
   cudaMemcpy(c, c_device, sizeof(c), cudaMemcpyDeviceToHost);
   for (int i = 0; i < N; i++) {
     printf("%.2f\n", c[i]);
--- myapps/vector_add.sh
+++ myapps/vector_add.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-nvcc -o vector_add vector_add.cu
+pushd ./$1
+rm -rf vector_add &&\
+nvcc -o vector_add vector_add.cu &&\
 ./vector_add
+popd
